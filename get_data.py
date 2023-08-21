9a9924e40c8bb4c10466213d7230b45865940f59
jiho-par <hkp314518@duksung.ac.kr>
2023-08-21 19:24:46 +0900

    추천 - memberid, genre

--- get_data.py
+++ get_data.py
@@ -1,3 +1,6 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from konlpy.tag import Okt
 import mysql.connector
 
 # MySQL 데이터베이스 연결 설정
@@ -9,6 +12,13 @@ db_config = {
     "port": 3306          # MySQL 포트 번호
 }
 
+# KoNLPy의 Okt 객체 생성
+okt = Okt()
+
+# 텍스트 전처리 및 토큰화 함수
+def preprocess_text(text):
+    words = okt.morphs(text, stem=True)
+    return ' '.join(words)
 
 # "item" 테이블 데이터 가져오기
 def get_item_data():
@@ -40,14 +50,56 @@ def get_chat_data():
 
     return chat_data
 
+def recommend_movies_for_members(item_data, chat_data):
+    # 영화 상세 설명 데이터
+    movie_descriptions = [item[1] for item in item_data]
+    
+    # 고유한 멤버 ID 가져오기
+    member_ids = set(chat[0] for chat in chat_data)
+
+    # 각 멤버의 채팅 메시지를 저장할 딕셔너리
+    member_chat_messages = {}
+
+    # 채팅 데이터를 멤버별로 그룹화하여 처리
+    for chat in chat_data:
+        member_id, message = chat[0], chat[1]
+        if member_id not in member_chat_messages:
+            member_chat_messages[member_id] = []
+        member_chat_messages[member_id].append(message)
+
+    # 멤버별 추천 영화를 저장하는 딕셔너리
+    recommended_movies = {}
+
+    # 멤버별 채팅 데이터를 가져와서 처리
+    for member_id in member_ids:
+        chat_messages = member_chat_messages.get(member_id, [])
+
+        # 데이터 전처리
+        preprocessed_chat_messages = [preprocess_text(text) for text in chat_messages]
+        preprocessed_movie_descriptions = [preprocess_text(text) for text in movie_descriptions]
+
+        # TF-IDF 벡터화
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(preprocessed_chat_messages + preprocessed_movie_descriptions)
+
+        # 채팅 메시지와 영화 설명 간의 코사인 유사도 계산
+        similarity_matrix = cosine_similarity(tfidf_matrix)
+
+        # 유사도가 높은 영화 추천
+        chat_similarity_scores = similarity_matrix[:-len(movie_descriptions), -len(movie_descriptions):]
+        top_similar_indices = chat_similarity_scores.argmax(axis=1)
+
+        recommended_movies[member_id] = [item_data[idx][1] for idx in top_similar_indices]
+
+        return recommended_movies
+
 if __name__ == "__main__":
     item_data = get_item_data()
     chat_data = get_chat_data()
 
-    print("Item Data:")
-    for item in item_data:
-        print(item)
+    recommended_movies = recommend_movies_for_members(item_data, chat_data)
 
-    print("\nChat Data:")
-    for chat in chat_data:
-        print(chat)
+    for member_id, movies in recommended_movies.items():
+        print(f"{member_id} 멤버에게 추천하는 영화:")
+        for movie in movies:
+            print(movie)
